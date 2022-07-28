f4f504f83cb047b08a9f39fc033512363220a6e0
kapiosk <29824083+kapiosk@users.noreply.github.com>
2022-07-28 17:44:12 +0300

    init

---
+++ BlazorFix.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace _3ai.solutions.BlazorSubdirectoryFix
+{
+    public static class BlazorFix
+    {
+        public static IApplicationBuilder UseBlazorFix(this IApplicationBuilder app)
+        {
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.Path.HasValue && context.Request.Path.Value.Contains("_content") && !context.Request.Path.Value.StartsWith("/_"))
+                {
+                    string url = context.Request.Path.Value;
+                    url = url[url.IndexOf("_")..];
+                    context.Response.Redirect($"/{url}");
+                }
+                else
+                    await next.Invoke();
+            });
+            return app;
+        }
+    }
+}
